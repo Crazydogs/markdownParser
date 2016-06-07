b2383f219727c805ced59bc585d6c4901325e069
yueming <crazydogazrael@gmail.com>
2016-06-07 20:04:38 +0800

    setp 1

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var process = require('process');
 var _ = require('underscore-node');
 
+var markdownParser = require('./parser.js');
+
 var fileName = process.argv[2];
 
 if(!fileName) {
@@ -21,95 +23,9 @@ var getOriginalText = new Promise(function(resolve, reject) {
 });
 
 getOriginalText.then(function(data) {
-    console.log(
-        splitByEmptyRow(data)
-            .map(parseBlockType)
-    );
+    console.log(markdownParser.parse(data));
+        //splitByEmptyRow(data)
+        //    .map(parseBlockType)
 }).catch(function (err) {
     console.log(err);
 });
-
-function splitByEmptyRow(data) {
-    return data
-        .split('\n')
-        .reduce(function(preVal, currentVal){
-            if (currentVal) {
-                preVal[preVal.length - 1].push(currentVal);
-            } else {
-                if (preVal[preVal.length - 1].length != 0) {
-                    preVal.push([]);
-                }
-            }
-            return preVal;
-        }, [[]])
-        .slice(0, -1);
-}
-
-function parseBlockType(block) {
-    return _.map(blockReconizer, function(fun, name) {
-        if(fun(block)) {
-            return {
-                type: name,
-                data: block
-            }
-        } else {
-            return false;
-        }
-    }).find(function(result) {
-        return result;
-    }) || {
-        type: 'normal',
-        data: block
-    };
-}
-
-
-var regex = {
-    list: /^\d+\. /,
-    subList: /^(    |\t)\d+\. /,
-    subListContent: /^(    |\t){2}/
-}
-
-var blockReconizer = {
-    listBlock: function(block) {
-        return block.every(function(row, rowIndex) {
-            return rowIndex ? /^(\d+\. |    |\t)/.test(row)
-                            : /^\d+\. /.test(row);
-        });
-    },
-    unorderedListBlock: function(block) {
-        return block.every(function(row, rowIndex) {
-            return rowIndex ? /^(- |    |\t).*/.test(row)
-                            : /^- /.test(row);
-        });
-    },
-    codeBlock: function(block) {
-        return block.length >= 3
-                && /^`{4}/.test(block[0])
-                && /^`{4}/.test(block[block.length - 1]);
-    }
-};
-
-var listParser = function(block) {
-    _.map(block, function(row, rowIndex) {
-        if(/^(    |\t)\d+\. /.test(row)) {
-            return {
-                type: 1,
-                data: row
-            }
-        }
-        if(/^(    |\t){2}/.test(row)) {
-            return {
-                type: 2,
-                data: row
-            }
-        }
-        if()
-        return row;
-    });
-}
-
-
-var inlineReconizer = {
-    
-};
--- parser.js
+++ parser.js
@@ -1,14 +1,102 @@
+var _ = require('underscore-node');
+
+var markdownParser = {
+    parse: function (originStr) {
+        return splitByEmptyRow(originStr).map(parseBlockType);
+    }
+} 
+
+
+/*
+ *  根据空行进行切片
+ *  in: data 原始字符串
+ *  out: 段落数组，每个段落也是数组，包含段落内的行
+ */
+function splitByEmptyRow(data) {
+    return data
+        .split('\n')
+        .reduce(function(preVal, currentVal){
+            if (currentVal) {
+                preVal[preVal.length - 1].push(currentVal);
+            } else {
+                if (preVal[preVal.length - 1].length != 0) {
+                    preVal.push([]);
+                }
+            }
+            return preVal;
+        }, [[]])
+        .slice(0, -1);
+}
+
+/*
+ *  段落分类
+ *  in: 段落，如 ['段内第一行', '段内第二行']
+ *  out: 带有段落类型的段落数组，段落格式如下
+ *      {
+ *          type: listBlock,
+ *          data: ['段内第一行', '段内第二行']
+ *      }
+ */
+function parseBlockType(block) {
+    return _.map(blockReconizer, function(fun, name) {
+        if(fun(block)) {
+            return {
+                type: name,
+                data: block
+            }
+        } else {
+            return false;
+        }
+    }).find(function(result) {
+        return result;
+    }) || {
+        type: 'normal',
+        data: block
+    };
+}
+
+/*
+ * 正则表达式
+ */
 var regex = {
+    // 有序列表
     list: /^\d+\. /,
     subList: /^(    |\t)\d+\. /,
     subListContent: /^(    |\t){2}/,
-
+    // 无序列表
     unorderedList: /^- /,
     subUnorderedList: /^(    |\t)- /,
     subUnorderedListContent: /^(    |\t){2}/,
+    // 代码
+    codeBorder: /^`{4}/,
+    // 缩进
+    indent: /^(    |\t)/
 }
 
-var markdownParser = {
-}
+/*
+ *  段落类型匹配器
+ */
+var blockReconizer = {
+    // 有序列表
+    listBlock: function(block) {
+        return block.every(function(row, rowIndex) {
+            return rowIndex ? (regex.list.test(row) || regex.indent.test(row))
+                            : regex.list.test(row);
+        });
+    },
+    // 无序列表
+    unorderedListBlock: function(block) {
+        return block.every(function(row, rowIndex) {
+            return rowIndex ? (regex.unorderedList.test(row) || regex.indent.test(row))
+                            : regex.unorderedList.test(row);
+        });
+    },
+    // 代码
+    codeBlock: function(block) {
+        return block.length >= 3
+                && regex.indent.test(block[0])
+                && regex.indent.test(block[block.length - 1]);
+    }
+};
 
 module.exports = markdownParser;
